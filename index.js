30721b9bf0013f50c88c377c2facc18f3d3efb10
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-04-09 19:03:22 +0100

    Give both functions names

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer()
 server.on("request", function handleRequest(req, res) {
 	res.end()
 })
-server.listen(3999, function() {
+server.listen(3999, function logSuccessSettingUpServer() {
 	console.log("Listening on port 4000 for incoming requests.")
 	console.log("Try visiting http://localhost:4000 in your browser")
 })
